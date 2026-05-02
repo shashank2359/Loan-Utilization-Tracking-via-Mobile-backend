4e7d49e989626666f6a913f9ab23c06f7c535235
Shashank Singh Sengar <shashanksengar238@gmail.com>
2026-05-02 02:13:29 -0700

    added debug logs

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION:", err);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION:", err);
+});
+
+console.log("Starting server...");
+
 require("dotenv").config();
+console.log("Env loaded");
+
 const app = require("./src/app");
+console.log("App loaded");
+
 const mongoose = require("mongoose");
+console.log("Mongoose loaded");
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,13 +23,6 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 5000
-})
-.then(() => {
-  console.log("MongoDB Connected Successfully");
-})
-.catch((err) => {
-  console.log("FULL ERROR BELOW:");
-  console.log(err);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("DB ERROR:", err));
